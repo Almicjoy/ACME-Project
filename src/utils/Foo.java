0992f0b8be439a8927922bd98cf2586bdf3674c1
Alessa Castillo <amcastillo@valdosta.edu>
2022-08-21 16:51:50 -0400

    Update Foo from person2

---
+++ src/utils/Foo.java
@@ -0,0 +1,14 @@
+package utils;
+
+public class Foo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Howdy");
+		System.out.println("Hi from GitHub");
+		System.out.println("Hi alessa1 branch, from Eclipse");
+		System.out.println("Hello Worlds");
+		System.out.println("Hello from person2");
+	}
+
+}
